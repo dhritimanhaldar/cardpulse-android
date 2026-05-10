4548080784de21d756634c26d3d46db6e618acd1
Dhritiman Haldar <dhritiman.haldar@gmail.com>
2026-05-10 22:36:38 +0530

    chore: gradle issue fix - 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,19 @@ android {
         compose = true
         buildConfig = true
     }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/DEPENDENCIES"
+            excludes += "/META-INF/LICENSE"
+            excludes += "/META-INF/LICENSE.txt"
+            excludes += "/META-INF/license.txt"
+            excludes += "/META-INF/NOTICE"
+            excludes += "/META-INF/NOTICE.txt"
+            excludes += "/META-INF/notice.txt"
+            excludes += "/META-INF/*.kotlin_module"
+        }
+    }
 }
 
 dependencies {

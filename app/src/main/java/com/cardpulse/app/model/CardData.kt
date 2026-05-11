5f48cf2297c0aaf061b54429d2e14fe69830ac2d
Dhritiman Haldar <dhritiman.haldar@gmail.com>
2026-05-11 21:06:37 +0530

    Fixes - try - 6

--- app/src/main/java/com/cardpulse/app/model/CardData.kt
+++ app/src/main/java/com/cardpulse/app/model/CardData.kt
@@ -3,14 +3,41 @@ package com.cardpulse.app.model
 import com.google.gson.annotations.SerializedName
 
 data class CardDataRoot(val v: Int, val banks: List<BankEntry>)
+
 data class BankEntry(val b: String, val bn: BinMetadata?, val p: List<Perk>?, val m: List<Milestone>?, val g: List<GroupEntry>?)
+
 data class GroupEntry(val n: String, val bn: BinMetadata?, val p: List<Perk>?, val m: List<Milestone>?, val c: List<CardEntry>?, val x: List<String>?)
+
 data class CardEntry(val n: String, val nw: String?, val t: String?, val af: Int?, val jf: Int?, val fs: Int?, val fm: Double?, val rv: Double?, val cl: String?, val cf: String?, val bn: BinMetadata?, val p: List<Perk>?, val m: List<Milestone>?, val cv: List<String>?, val x: List<String>?)
+
 data class BinMetadata(val px: List<String>?, val rg: List<BinRange>?, val ln: List<Int>?)
+
 data class BinRange(val f: String, val t: String)
-data class Perk(val i: String, val n: String, val rt: String, val cy: String, val mn: Int?, val up: UptoLimit?, val md: PerkModification?, val x: List<String>?)
+
+data class Perk(
+    val i: String,
+    val n: String,
+    val rt: String,
+    val cy: String,
+    val mn: Int?,
+    val up: UptoLimit?,
+    @SerializedName("md") val md: PerkModification? = null,
+    val x: List<String>?,
+    val vp: Int? = null,
+    val nw: String? = null
+)
+
 data class UptoLimit(val t: String, val v: Int)
-data class PerkModification(val mn: Int?, val up: UptoLimit?, val x: List<String>?)
+
+data class PerkModification(
+    val mn: Int? = null,
+    val up: UptoLimit? = null,
+    val x: List<String>? = null,
+    val n: String? = null,
+    val vp: Int? = null,
+    val cy: String? = null
+)
+
 data class Milestone(
     val i: String,
     val n: String,
@@ -19,4 +46,5 @@ data class Milestone(
     val rt: String,
     val cy: String
 )
+
 data class ResolvedCard(val bankName: String, val groupName: String, val cardName: String, val cardNetwork: String, val cardType: String, val annualFee: Int, val joiningFee: Int, val feeWaiverSpend: Int, val foreignMarkup: Double, val rewardValue: Double, val color: String, val confidence: String, val perks: List<Perk>, val milestones: List<Milestone>, val caveats: List<String>)
